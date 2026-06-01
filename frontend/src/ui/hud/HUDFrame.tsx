251309faa7489bf8724bf5ec537d55e7cb04ab98
AstarophTY <astarophty@gmail.com>
2026-06-01 17:00:00 +0200

    refactor: Remove uncomment

--- frontend/src/ui/hud/HUDFrame.tsx
+++ frontend/src/ui/hud/HUDFrame.tsx
@@ -1,13 +1,13 @@
 import PlanetQuickSelect from "@/components/hud/PlanetQuickSelect";
 import SocialPanel from "@/components/hud/SocialPanel";
 import EditorMode from "@/components/hud/editor/EditorMode";
-import AuthPanel from "@/ui/hud/AuthPanel"
+// import AuthPanel from "@/ui/hud/AuthPanel"
 
 export default function HUDFrame() {
   return (
     <>
         <SocialPanel />
-        <AuthPanel />
+        {/* <AuthPanel /> */}
         <EditorMode />
         <PlanetQuickSelect />
       </>
