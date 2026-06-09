f06d2166448144f6430b0d1577762eaccb6a0345
tristan-gscn <tristan.gscn@proton.me>
2026-06-09 16:54:07 +0200

    Fix claim zone visual indicator

--- frontend/src/ui/hud/HUDFrame.tsx
+++ frontend/src/ui/hud/HUDFrame.tsx
@@ -6,18 +6,14 @@ import MobileVirtualControls from "@/ui/hud/MobileVirtualControls"
 import TakeoffOverlay from "@/ui/hud/TakeoffOverlay"
 import { usePlanetStore } from '@/store/planetStore'
 import { useEditorStore } from '@/store/editorStore'
-import { Badge } from "@/ui/shadcn/badge"
-
 export default function HUDFrame() {
   const sceneMode = usePlanetStore(state => state.sceneMode);
   const editorMode = useEditorStore(state => state.in_editor);
-  const inClaimZone = useEditorStore((state) => state.inClaimZone);
   return (
     <>
        {sceneMode === 'selection' ? <PlanetQuickSelect /> : <></>}
         <SocialPanel />
         <AuthPanel />
-        {inClaimZone ?<Badge className="fixed top-6 left-1/2 -translate-x-1/2 bg-green-50 text-green-700 dark:bg-green-950 dark:text-green-300">ZONE CLAIM</Badge> : <></>}
         {editorMode ? <EditorMode /> : <></>}
         {sceneMode === 'world' && <MobileVirtualControls />}
         <TakeoffOverlay />
