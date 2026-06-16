2f682cf6e60f162b25b5b8f4f666c4d379e909d8
tristan-gscn <tristan.gscn@proton.me>
2026-06-16 15:07:06 +0200

    Fix issues after merge

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -2,17 +2,4 @@
 
 set -e
 
-cp ./scripts/auto_campus_vote_routine.sh /opt/auto_campus_vote_routine.sh
-chmod +x /opt/auto_campus_vote_routine.sh
-
-cp ./scripts/campus_process_winner.sh /opt/campus_process_winner.sh
-chmod +x /opt/campus_process_winner.sh
-
-cp ./scripts/process_all_winners.sh /opt/process_all_winners.sh
-chmod +x /opt/process_all_winners.sh
-
-# crond removed
-#crontab ./scripts/vote.crontab
-#crond &
-
 exec node dist/main.js
